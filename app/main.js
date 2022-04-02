1e1fe353ded1c6ee0429eea3dd524f10e24f6a52
SayudhaStudio <mazaya.dev@gmail.com>
2022-04-02 19:26:28 +0700

    + add method function

--- app/main.js
+++ app/main.js
@@ -11,5 +11,13 @@ const upvoteApp = {
 			});
 		},
 	},
+	methods: {
+		upvote(submissionId) {
+			const submission = this.submissions.find(
+				(submission) => submission.id === submissionId
+			);
+			submission.votes++;
+		},
+	},
 };
 Vue.createApp(upvoteApp).mount('#app');
